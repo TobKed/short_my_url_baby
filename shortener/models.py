55a589baf864f70048e37e5f544b36269deacd14
TobKed <tobiaszkedzierski@gmail.com>
2018-12-22 15:43:05 +0100

    add and register Link model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    url         = models.URLField(max_length=2056)
+    link_id     = models.CharField(max_length=10, unique=True)
